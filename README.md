1e91225f577e053ea050c9d9e1928db65d510308
lokeswaranaidu <246lokesh@gmail.com>
2024-02-04 11:24:19 +0530

    updates on Emetion_Detection

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Emotion-Detection
-Emotion Detection
+''' bash
+conda create -n Emotion Detection python=3.8 -y
+
+
+'''
+
+
+''' bash
+conda activate Emotion Detection
+
+
+'''
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name="Emotion Detection"
+project_name="Emotion_Detection"
 
 
 list_of_files = [
